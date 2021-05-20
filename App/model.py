bc6b71fe206b1e84f5ce8c807784d3d247db72e6
jcalixto202020774 <j.calixto@uniandes.edu.co>
2021-05-19 23:52:43 -0500

    Avance carga de datos, este reto es un sufrimiento y me quiero ir a dormir

--- App/model.py
+++ App/model.py
@@ -25,7 +25,9 @@
  """
 
 
+from os import name
 import config as cf
+import math
 from DISClib.ADT import list as lt
 from DISClib.ADT import map as mp
 from DISClib.ADT.graph import gr
@@ -46,8 +48,9 @@ def newCatalog():
                }
 
     catalog['connections'] = gr.newGraph(datastructure='ADJ_LIST',
-                                         directed = True,
-                                         size= 15000)
+                                         directed = False,
+                                         size= 15000,
+                                         comparefunction=compareJointId)
     
     catalog['landing_points'] = mp.newMap(numelements=15000,
                                           maptype='PROBING')
@@ -59,12 +62,68 @@ def newCatalog():
 
 # Construccion de modelos
 
+def addCable(catalog, cable):
+    origin = cable['origin']
+    destination = cable['destination']
+    ori_couple = mp.get(catalog['landing_points'],origin)
+    ori_coor = me.getValue(ori_couple)
+    des_couple = mp.get(catalog['landing_points'],destination)
+    des_coor = me.getValue(des_couple)
+    distance = haversine (float(ori_coor['latitude']),float(ori_coor['longitude']),float(des_coor['latitude']),float(des_coor['longitude']))
+
+    name_ori = formatVertex(cable['origin'],cable['cable_name'])
+    name_des = formatVertex(cable['destination'],cable['cable_name'])
+    addJoint(catalog,name_ori)
+    addJoint(catalog,name_des)
+    addConnection(catalog, name_ori, name_des, distance)
+
+
 # Funciones para agregar informacion al catalogo
 
 # Funciones para creacion de datos
+
+def haversine(lat_1,lon_1,lat_2,lon_2):
+    radius = 6356 
+    delta_lat = math.radians(lat_2) - math.radians(lat_1)
+    delta_lon = math.radians(lon_2) - math.radians(lon_1)
+    
+
+    a = (math.sin(delta_lat/2))**2 + math.cos(lat_1)*math.cos(lat_2)*(math.sin(delta_lon/2))**2
+    c = 2*math.asin(a**(1/2))
+    d = radius * c
+
+    return d
+
+def addLandingPoint(catalog, landing_point):
+    mp.put(catalog['landing_points'],landing_point['landing_point_id'],landing_point)
+
+def formatVertex(origin, name):
+    format = origin + '-' + name
+    return format
+
+def addJoint(catalog, vertex):
+    if not gr.containsVertex(catalog['connections'],vertex):
+        gr.insertVertex(catalog['connections'],vertex)
+
+def addConnection(catalog, origin, destination, distance):
+    edge = gr.getEdge(catalog['connections'],origin,destination)
+    if edge is None:
+        gr.addEdge(catalog['connections'],origin, destination,distance)
+
 
 # Funciones de consulta
 
 # Funciones utilizadas para comparar elementos dentro de una lista
 
 # Funciones de ordenamiento
+
+def compareJointId(stop, keyvaluestop):
+    stopcode = keyvaluestop['key']
+    if (stop == stopcode):
+        return 0
+    elif (stop >stopcode):
+        return 1
+    else:
+        return -1
+
+print(haversine(4.716165657240686, -74.049249231947, 5.780219354367528, -73.12374102667047))
